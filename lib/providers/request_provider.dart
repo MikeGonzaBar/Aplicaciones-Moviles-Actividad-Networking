d0eef0ab2d79c71eb16d81b31ace423c8fcb603a
Miguel González Barajas <is722298@iteso.mx>
2022-10-06 08:43:48 -0500

    feat: added provider

---
+++ lib/providers/request_provider.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class RequestProvider with ChangeNotifier {
+  List<dynamic> _reqList = [];
+  dynamic _resp = {};
+  final _usersEndpoint = "https://jsonplaceholder.typicode.com/posts";
+  List<dynamic> get getReqList => _reqList;
+
+  Future<dynamic> getUsers() async {
+    try {
+      var response = await http.get(Uri.parse(_usersEndpoint));
+      if (response.statusCode == 200) {
+        var content = jsonDecode(response.body);
+        _reqList = content;
+        notifyListeners();
+      }
+    } catch (e) {
+      _reqList = [];
+      notifyListeners();
+    }
+  }
+
+  Future<dynamic> postToAPI(dynamic data) async {
+    try {
+      Map<String, String> body = {
+        'title': '${data.title}',
+        'body': '${data.body}',
+        'userId': '${data.userId}',
+      };
+      var response = await http.post(Uri.parse(_usersEndpoint), body: body);
+      if (response.statusCode == 200) {
+        var content = jsonDecode(response.body);
+        _resp = content;
+        notifyListeners();
+      }
+    } catch (e) {
+      _resp = {};
+      notifyListeners();
+    }
+  }
+}
